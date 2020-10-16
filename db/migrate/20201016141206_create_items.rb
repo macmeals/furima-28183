690fb742e554fe2996fd73592a1375b2c5323479
macmeals <loseworld9731@gmail.com>
2020-10-16 23:42:01 +0900

    itemsテーブル作成

---
+++ db/migrate/20201016141206_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string   :name  ,   null: false
+      t.integer  :price ,  null: false
+      t.text     :explaination , null: false
+      t.integer  :category_id , null: false
+      t.integer  :status_id ,  null: false
+      t.integer  :ship_cost_id , null: false
+      t.integer  :ship_loction_id  , null: false
+      t.integer  :ship_day_id , null: false
+      t.integer  :user_id , null: false , foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
