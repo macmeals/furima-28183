760f84e4c6ca5f34e2f0345884e98a0011ca5271
macmeals <loseworld9731@gmail.com>
2020-10-17 17:20:04 +0900

    categoryのActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
-  - belongs_to :user
-  - has_many :comments
-  - has_one :sold_item
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to :user
+  has_many :comments
+  has_one :sold_item
+  has_one_attached :image
+
+  #categoryの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
+
 end
