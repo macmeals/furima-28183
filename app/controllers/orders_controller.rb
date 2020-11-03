44846f8f465d4ca88715b046490964597c9acb07
macmeals <loseworld9731@gmail.com>
2020-11-04 06:28:23 +0900

    コントローラーアクション実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    
+    @order = Order.new
   end
 
   def create
-    
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to orders_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code ,:prefecture_id ,:city ,:house_address ,:building ,:tell_number ,:sold_item_id)
+  
   end
 
 end
