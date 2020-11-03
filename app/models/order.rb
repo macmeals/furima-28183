8ba4e565d5347f750c1d417c3d031f52455d0855
macmeals <loseworld9731@gmail.com>
2020-11-03 17:49:50 +0900

    バリデーション実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+  belong_to :sold_item
+
+  with_options presence: true do
+    validates :post_code , format:{ with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :city
+    validates :house_address
+    validates :tell_number , format:{ with:/\A[0-9]+\z/},length: { maximum: 11 }
+    
+  end
 end
